136768d64dd15683f13040acbfabf8b49fe95681
Samuel Garcia <samg15wr@gmail.com>
2017-10-15 17:13:45 +0200

    Ajout de la page Accueil
    + lien de la connexion jusqu'à accueil

---
+++ app/src/main/java/com/example/stri/virtualbank/Accueil.java
@@ -0,0 +1,19 @@
+package com.example.stri.virtualbank;
+
+/**
+ * Created by Samuel on 13/10/2017.
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Accueil extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+    }
+}
--- app/src/main/java/com/example/stri/virtualbank/connexion.java
+++ app/src/main/java/com/example/stri/virtualbank/connexion.java
@@ -1,7 +1,10 @@
 package com.example.stri.virtualbank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Connexion extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class Connexion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        final Button btn_connect = (Button) findViewById(R.id.id_btn_connect);
+
+        btn_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //TODO Ajouter les contrôles de compte
+
+                Intent intent = new Intent(Connexion.this, Accueil.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
