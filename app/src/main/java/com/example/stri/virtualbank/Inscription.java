eb5da02d8cfed29281f376553c7283290f9dcbec
Samuel Garcia <samg15wr@gmail.com>
2017-10-17 14:56:31 +0200

    Ajout de la page Inscription
    + Détail de la page connexion

---
+++ app/src/main/java/com/example/stri/virtualbank/Inscription.java
@@ -0,0 +1,30 @@
+package com.example.stri.virtualbank;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Inscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+        getSupportActionBar().hide();
+
+        final Button btn_inscription_valider = (Button) findViewById(R.id.id_btn_inscription_valider);
+
+
+        btn_inscription_valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View s) {
+                Intent intent = new Intent(Inscription.this, Connexion.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/stri/virtualbank/connexion.java
+++ app/src/main/java/com/example/stri/virtualbank/connexion.java
@@ -11,9 +11,12 @@ public class Connexion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_connexion);
 
         final Button btn_connect = (Button) findViewById(R.id.id_btn_connect);
+        final Button btn_inscription = (Button) findViewById(R.id.id_btn_inscription);
+
 
         btn_connect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +25,17 @@ public class Connexion extends AppCompatActivity {
                 //TODO Ajouter les contrôles de compte
 
                 Intent intent = new Intent(Connexion.this, Accueil.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //TODO Ajouter les contrôles de compte
+
+                Intent intent = new Intent(Connexion.this, Inscription.class);
                 startActivity(intent);
             }
         });
