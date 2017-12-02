10789a277bf45eeee7acbbaf3255a56bf283bb77
Samuel Garcia <samg15wr@gmail.com>
2017-12-02 16:57:34 +0100

    Connexion fonctionnel

--- app/src/main/java/com/example/stri/virtualbank/connexion.java
+++ app/src/main/java/com/example/stri/virtualbank/connexion.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class connexion extends AppCompatActivity {
 
@@ -23,9 +24,19 @@ public class connexion extends AppCompatActivity {
             public void onClick(View view) {
 
                 //TODO Ajouter les contrôles de compte
+                String mail = new String();
+                String mdp = new String();
 
-                Intent intent = new Intent(connexion.this, Accueil.class);
-                startActivity(intent);
+                mail = "mail";
+                mdp = "pass";
+
+                EditText champ_mail = (EditText) findViewById(R.id.id_text_mail);
+                EditText champ_mdp = (EditText) findViewById(R.id.id_text_mdp);
+
+                if (mail.compareTo(champ_mail.getText().toString()) == 0 && mdp.compareTo(champ_mdp.getText().toString()) == 0) {
+                    Intent intent = new Intent(connexion.this, Accueil.class);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -34,6 +45,7 @@ public class connexion extends AppCompatActivity {
             public void onClick(View view) {
 
                 //TODO Ajouter les contrôles de compte
+
 
                 Intent intent = new Intent(connexion.this, Inscription.class);
                 startActivity(intent);
